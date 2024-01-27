41a11f821edf6be90d0c31002dc84dcab4571239
Victoria Lang <victoria@pluralpolicy.com>
2024-01-27 16:24:09 -0500

    Add link to source code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import { ReactElement } from "react";
 const Footer = (): ReactElement => {
     return (
         <div>
-            Favicon by <a href="https://twemoji.twitter.com/">Twemoji</a>
+            <p>Favicon by <a href="https://twemoji.twitter.com/">Twemoji</a></p>
+            <p>Source code available <a href="https://github.com/PapaJohnsGarlicSauce/PapaJohnsGarlicSauce.github.io/tree/main">here</a></p>
         </div>
     );
 };
