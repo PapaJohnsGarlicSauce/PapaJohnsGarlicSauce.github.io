b6ee6d3875d310136a2a8c92b83cbde0a16c8cd3
Victoria Lang <victoriamichelelang@gmail.com>
2024-02-01 19:57:18 -0500

    Style hover

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -27,23 +27,31 @@ const Projects = (): ReactElement => {
       <h2>Projects</h2>
       <p>
         Here are some selected projects on Github, mostly me messing around with
-        stuff. A more detailed project outline can be found on my{" "}
-        <Link to={"/resume"}> resume</Link>
+        stuff and getting cofortable with different frameworks and paradigms. A
+        more detailed project outline can be found on my{" "}
+        <Link to={"/resume"}> resume</Link>.
       </p>
       <Grid container spacing={2}>
         {repos?.map((repo: any) => (
           <Grid item xs={12} sm={6} md={4} key={repo.id}>
-            <Card>
-              <CardContent>
-                <Typography sx={{ fontSize: 16 }} gutterBottom>
-                  {repo.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {repo.description}
-                </Typography>
-                {repo.language}
-              </CardContent>
-            </Card>
+            <a
+              href={repo.html_url}
+              target="_blank"
+              rel="noreferrer"
+              className="card-link"
+            >
+              <Card variant="outlined">
+                <CardContent>
+                  <Typography sx={{ fontSize: 16 }} gutterBottom>
+                    {repo.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {repo.description ?? "No description"}
+                  </Typography>
+                  {repo.language ?? "N/A"}
+                </CardContent>
+              </Card>
+            </a>
           </Grid>
         ))}
       </Grid>
